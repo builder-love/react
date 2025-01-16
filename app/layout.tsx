f33e613d2606e670a045fd3d1f7eeafdccd1f243
treboryatska <96736677+treboryatska@users.noreply.github.com>
2025-01-16 08:58:11 -0500

    footer with social links
    added footer and moved all social links there. Added link to docs

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
 import Navigation from './navigation';
+import Footer from './footer';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -17,7 +18,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} bg-black text-white`}>
+      <body className={`${inter.className} bg-black text-white flex flex-col min-h-screen`}>
         <Navigation />
         <main className="flex-1 ml-64 p-5">
           <h1 className="text-3xl font-bold text-red-500 text-center mt-5">
@@ -29,6 +30,7 @@ export default function RootLayout({
           </p>
           {children}
         </main>
+        <Footer />
       </body>
     </html>
   );
