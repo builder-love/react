34269e4a99a66795206e38d7fc869e246a0db880
treboryatska <96736677+treboryatska@users.noreply.github.com>
2025-05-15 07:11:15 -0400

    tailwind safelist

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -15,4 +15,14 @@ export default {
     },
   },
   plugins: [],
+  // safelist for grid-cols-1 to grid-cols-7
+  safelist: [
+    'grid-cols-1',
+    'grid-cols-2',
+    'grid-cols-3',
+    'grid-cols-4',
+    'grid-cols-5',
+    'grid-cols-6',
+    'grid-cols-7',
+  ],
 } satisfies Config;
