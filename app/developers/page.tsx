a41cbfbbbc80827980824b8f77225831bdc8f12f
treboryatska <96736677+treboryatska@users.noreply.github.com>
2025-05-16 08:53:52 -0400

    rm comments

--- app/developers/page.tsx
+++ app/developers/page.tsx
@@ -41,39 +41,6 @@ const multiSelectFilter: FilterFn<Top100Contributor> = (row, columnId, filterVal
   return filterValue.includes(value);
 };
 
-// interface CustomCheckboxProps {
-//   checked: boolean;
-//   onChange: () => void;
-// }
-
-// const CustomCheckbox: React.FC<CustomCheckboxProps> = ({ checked, onChange }) => {
-//   return (
-//     <div
-//       className={`w-4 h-4 border rounded cursor-pointer flex items-center justify-center ${
-//         checked ? "bg-blue-600 border-blue-600" : "bg-gray-800 border-gray-300"
-//       }`}
-//       onClick={onChange}
-//     >
-//       {checked && (
-//         <svg
-//           className="w-3 h-3 text-white"
-//           fill="none"
-//           stroke="currentColor"
-//           viewBox="0 0 24 24"
-//           xmlns="http://www.w3.org/2000/svg"
-//         >
-//           <path
-//             strokeLinecap="round"
-//             strokeLinejoin="round"
-//             strokeWidth={2}
-//             d="M5 13l4 4L19 7"
-//           />
-//         </svg>
-//       )}
-//     </div>
-//   );
-// };
-
 const ContributorsPage: React.FC = () => {
   const [data, setData] = useState<Top100Contributor[]>([]);
   const [isLoading, setIsLoading] = useState<boolean>(true);
